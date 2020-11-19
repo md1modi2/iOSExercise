ff6e7a0bd79ca60118e38f2733af67c45397fbd4
mehul.modi <mehul.modi@hiddenbrains.in>
2020-11-19 11:49:15 +0530

    added network call XCTest

--- Wipro/WiproTests/NetworkService.swift
+++ Wipro/WiproTests/NetworkService.swift
@@ -7,3 +7,59 @@
 //
 
 import Foundation
+
+class NetworkService {
+    
+    var session: URLSession!
+    
+    func getFacts(completion: @escaping (FactsModel?, Error?) -> Void) {
+        guard let url = URL(string: "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json")
+          else { fatalError() }
+        session.dataTask(with: url) { (data, response, error) in
+          guard error == nil else {
+            completion(nil, error)
+            return
+          }
+          guard let data = data else {
+           completion(nil, NSError(domain: "no data", code: 10, userInfo: nil))
+           return
+          }
+          let decodedValue = try! JSONDecoder().decode(FactsModel.self, from: data)
+          completion(decodedValue, nil)
+        }.resume()
+    }
+}
+
+class NetworkTask: URLSessionDataTask {
+  let data: Data?
+  let urlResponse: URLResponse?
+  let networkError: Error?
+
+  var completionHandler: ((Data?, URLResponse?, Error?) -> Void)?
+  init(data: Data?, urlResponse: URLResponse?, error: Error?) {
+    self.data = data
+    self.urlResponse = urlResponse
+    self.networkError = error
+  }
+  override func resume() {
+    DispatchQueue.main.async {
+        self.completionHandler?(self.data, self.urlResponse, self.networkError)
+    }
+  }
+}
+
+class NetworkSession: URLSession {
+    
+    private let task: NetworkTask
+    var url: URL?
+    
+    init(data: Data?, urlResponse: URLResponse?, error: Error?) {
+        task = NetworkTask(data: data, urlResponse: urlResponse, error:
+            error)
+    }
+    override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        self.url = url
+        task.completionHandler = completionHandler
+        return task
+    }
+}
--- Wipro/WiproTests/WiproTests.swift
+++ Wipro/WiproTests/WiproTests.swift
@@ -13,10 +13,12 @@ class WiproTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
     }
 
     func testExample() {
@@ -28,6 +30,21 @@ class WiproTests: XCTestCase {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
+        }
+    }
+    
+    func testGetFacts() {
+        let service = NetworkService()
+        let session  = NetworkSession(data: nil, urlResponse: nil, error: nil)
+        service.session = session
+        let errorExpectation = expectation(description: "error")
+        var errorResponse: Error?
+        service.getFacts { (response, error) in
+          errorResponse = error
+          errorExpectation.fulfill()
+        }
+        waitForExpectations(timeout: 1) { (error) in
+          XCTAssertNotNil(errorResponse)
         }
     }
 
